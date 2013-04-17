ae7b7e4e38b6371c6a022f11e962c2d135a85f6c
Chad Crissman <cdcrissman@gmail.com>
2013-04-17 13:57:16 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem "json"
+gem "sinatra"
+gem "sinatra_auth_github"
+gem "octokit"
---
+++ server.rb
@@ -0,0 +1,44 @@
+require 'sinatra/auth/github'
+require 'octokit'
+
+module Example
+  class MyBasicApp < Sinatra::Base
+
+    CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+    CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
+    enable :sessions
+
+    set :github_options, {
+      :scopes    => "repo",
+      :secret    => CLIENT_SECRET,
+      :client_id => CLIENT_ID,
+      :callback_url => "/",
+    }
+
+    register Sinatra::Auth::Github
+
+    get '/' do
+      if !authenticated?
+        authenticate!
+      else
+        client = Octokit::Client.new(
+          :login => github_user.login,
+          :oauth_token => github_user.token,
+          :auto_traversal => true
+        )
+        repos = client.organization_repositories('g5search', {:type => 'private'})
+        pull_requests = {
+          "g5-client-hub" => client.pull_requests("g5search/g5-client-hub"),
+          "g5-widget-garden" => client.pull_requests("g5search/g5-widget-garden"),
+        }
+        # repos.each do |repo|
+        #   if !client.pull_requests("g5search/#{repo.name}").empty?
+        #     pull_requests[repo.name] = client.pull_requests("g5search/#{repo.name}")
+        #   end
+        # end
+        erb :repos, :locals => {:repos => repos, :pull_requests => pull_requests}
+      end
+    end
+  end
+end
